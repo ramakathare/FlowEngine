91e33397f2d453b91a1f06347dc7a1ef8df6ac1e
Rama Kathare <ramakathare@gmail.com>
2017-11-07 00:08:46 +0530

    Initial Commit

--- flow-engine/src/App.tsx
+++ flow-engine/src/App.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'react-bootstrap';
 
-const logo = require('./logo.svg');
+//const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-4">
+                <Button bsSize="large" bsStyle="primary" className="col-md-12">Rules</Button>
+              </div>
+              <div className="col-md-4">
+                <Button bsSize="large" bsStyle="success" className="col-md-12">Data</Button>
+              </div>
+              <div className="col-md-4">
+                <Button bsSize="large" bsStyle="warning" className="col-md-12">Fire</Button>
+              </div>
+            </div>
+          </div>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
